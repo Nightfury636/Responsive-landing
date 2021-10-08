776853b73848cfa4988c961f00a57917c5a825ed
Nightfury636 <boxboxboxbox636@gmail.com>
2021-10-08 17:01:59 +0300

    add styles

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,3 @@ exports.watch = function () {
 
 
 
-<script>
-// When the user clicks on <div>, open the popup
-    function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
-</script>
